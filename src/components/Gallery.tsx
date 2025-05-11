4b1c70b7a4321e02c6c6eb08dbf30133c72f19c4
MyCah Pleasant <mycah@mac.fox-broadnose.ts.net>
2025-05-10 20:52:07 -0400

    Styling updates

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,9 @@
+import getGalleryImages from "../utils/getGalleryImages";
+
 function Gallery({ galleryName }: { galleryName: string }) {
-  return <h1>Gallery name: {galleryName}</h1>;
+  const galleryImages = getGalleryImages(galleryName);
+
+  return galleryImages.map((url) => <img src={url} />);
 }
 
 export default Gallery;
---
+++ src/utils/getGalleryImages.ts
@@ -0,0 +1,5 @@
+function getGalleryImages(galleryName: string): string[] {
+  return ["/surfers.jpg", "/surfers.jpg", "/surfers.jpg"];
+}
+
+export default getGalleryImages;

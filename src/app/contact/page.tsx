c7f79c0e37d5441eb3dcffea545db568d4924eac
MyCah Pleasant <mycahp@gmail.com>
2025-07-23 23:11:24 -0400

    Use formsubmit.co for forms

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,20 @@
 "use client";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 
 /* eslint-disable react/no-unescaped-entities */
 function Contact() {
-  const [submitted, setSubmitted] = useState(false);
+  const [success, setSuccess] = useState<string | null>(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setSuccess(params.get("success"));
+  }, []);
 
   return (
     <div className="flex justify-center bg-brand-off-white shadow-[0_-4px_8px_-4px_rgba(0,0,0,0.1)] mb-5 p-4 border-b-8 border-b-brand-teal rounded-t-2xl w-full h-full">
       <div className="flex flex-col justify-center gap-8 w-[90%] lg:w-[75%]">
-        {submitted && (
+        {success && (
           <div className="font-semibold text-brand-indigo">
             Thanks! Your message has been sent.
           </div>
@@ -23,12 +29,23 @@ function Contact() {
           </p>
         </div>
         <form
-          name="contact"
-          data-netlify="true"
-          onSubmit={() => setSubmitted(true)}
+          action="https://formsubmit.co/21a2d3821e2ccceec6574acda31f2e16"
+          method="POST"
           className="space-y-6 w-full font-body text-brand-navy"
         >
-          <input type="hidden" name="form-name" value="contact" />
+          {/* Optional hidden inputs */}
+          <input type="hidden" name="_captcha" value="true" />
+          <input type="hidden" name="_template" value="box" />
+          <input
+            type="hidden"
+            name="_autoresponse"
+            value="Thanks for your message! I’ll be in touch soon."
+          />
+          <input
+            type="hidden"
+            name="_next"
+            value="https://pleasant.photography/contact?success=true"
+          />
 
           <div>
             <label htmlFor="name" className="block mb-1 font-medium text-sm">
